ccf944fbeacae3d5b0bc58ff68c0fa394fcdeadc
NIKPATEL-13 <nikhilpatel26195@gmail.com>
2024-07-22 17:48:17 +0530

    fix: import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import PdfPrinter from './PdfPrinter';
+import Assets from './assets/Assets';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,10 +10,10 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={Assets.Logos.ViteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={Assets.Logos.ReactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
